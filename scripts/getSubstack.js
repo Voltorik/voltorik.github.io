c0459a9e859af19d3ab9205ffc19a2626f49a712
Jordan Tyler <jmattyler@hotmail.com>
2024-09-13 22:31:13 -0500

    Dynamic loads RSS feed to blog page

---
+++ scripts/getSubstack.js
@@ -0,0 +1,26 @@
+const url = 'https://corsproxy.io/?' + encodeURIComponent('https://voltorik.substack.com/feed'); 
+  fetch(url)
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    const items = data.querySelectorAll("item");
+    let html = "";
+    items.forEach(el => {
+      const title = el.querySelector("title").textContent;
+      const link = el.querySelector("link").textContent;
+      const pubDate = formatDate(el.querySelector("pubDate").textContent);
+        html += `<li class='substack-post'><a href="${link}" 
+            target="_blank">${title}</a> - ${pubDate}</div>`;
+    });
+    document.getElementById("substack-posts").innerHTML = html;
+  });
+  
+  function formatDate(rssDate) {
+    const date = new Date(rssDate);
+    const formattedDate = date.toLocaleDateString('en-US', {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit'
+    });
+    return formattedDate;
+  }
